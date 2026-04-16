1ccce5a1386db5da08e467d88d27f2451850d16f
Seja <seja.czstudio@gmail.com>
2026-04-16 17:54:48 +0200

    TimeSetter: implement widget methods

--- src/gui/time_widgets.cpp
+++ src/gui/time_widgets.cpp
@@ -1,6 +1,7 @@
 #include "gtkmm/enums.h"
 #include "gtkmm/spinbutton.h"
 #include "widgets.h"
+#include <climits>
 #include <vector>
 
 TimeSetter::TimeSetter()
@@ -9,8 +10,11 @@ TimeSetter::TimeSetter()
     a_minutes   (Gtk::Adjustment::create(0, 0, 59)),
     a_seconds   (Gtk::Adjustment::create(0, 0, 59)),
     
-    sep1(":"),
-    sep2(":")
+    sep1(" : "),
+    sep2(" : "), 
+    
+    min_s(0),
+    max_s(INT_MAX)
 {
     hours.set_adjustment(a_hours);
     minutes.set_adjustment(a_minutes);
@@ -25,6 +29,8 @@ TimeSetter::TimeSetter()
         sb -> set_size_request(50);
         sb -> add_css_class("title-2");
         sb -> set_margin(5);
+        sb -> signal_value_changed().connect(sigc::mem_fun(*this, &TimeSetter::update_adjustments));
+        sb -> signal_value_changed().connect(sigc::bind(sigc::mem_fun(*this, &TimeSetter::resolve_overflow), sb));
     }
     
     sep1.add_css_class("title-2");
@@ -43,3 +49,119 @@ TimeSetter::TimeSetter()
 
 TimeSetter::~TimeSetter()
 {}
+
+void TimeSetter::set_seconds(int seconds)
+{
+    std::array<int, 3> new_time = compute_time(seconds);
+    
+    // Nastavit čas do polí
+    if ((this -> min_s <= seconds) <= this -> max_s)
+    {
+        this -> hours.set_value(new_time[0]);
+        this -> minutes.set_value(new_time[1]);
+        this -> seconds.set_value(new_time[2]);
+    }
+    
+    // Aktualizovat limity
+    update_adjustments();
+}
+
+void TimeSetter::update_adjustments()
+{
+    auto current_time = get_time();
+    auto min_time = compute_time(this -> min_s);
+    auto max_time = compute_time(this -> max_s);
+    
+    int min_time_h = min_time[0];
+    int min_time_m = current_time[0] > min_time[0] ? 0 : min_time[1];
+    int min_time_s = current_time[0] > min_time[0] || current_time[1] > min_time[1] ? 0 : min_time[2];
+    
+    int max_time_h = max_time[0];
+    int max_time_m = current_time[0] < max_time[0] ? 59 : max_time[1];
+    int max_time_s = current_time[0] < max_time[0] || current_time[1] < max_time[1] ? 59 : max_time[2];
+    
+    a_hours -> set_lower(min_time_h);
+    a_minutes -> set_lower(min_time_m);
+    a_seconds -> set_lower(min_time_s);
+    
+    a_hours -> set_upper(max_time_h);
+    a_minutes -> set_upper(max_time_m);
+    a_seconds -> set_upper(max_time_s);
+}
+
+void TimeSetter::resolve_overflow(Gtk::SpinButton * widget)
+{
+    std::vector<Gtk::SpinButton * > sb_v = {&hours, &minutes, &seconds};
+    std::array<int, 3> min_time = compute_time(min_s);
+    std::array<int, 3> max_time = compute_time(max_s);
+    std::array<int, 3> * limit_ptr = nullptr;
+    
+    if (get_seconds() > this -> max_s) { limit_ptr = &max_time; }
+    else if (get_seconds() < this -> min_s) { limit_ptr = &min_time; }
+    else { return; }
+    
+    unsigned short i = 0;
+    for (Gtk::SpinButton * sb : sb_v)
+    {
+        if (sb == widget) { i++; continue; }
+        
+        sb -> set_value((*limit_ptr)[i]);
+        i++;
+    }
+}
+
+void TimeSetter::set_min_time(int seconds)
+{
+    this -> min_s = seconds;
+}
+
+void TimeSetter::set_max_time(int seconds)
+{
+    this -> max_s = seconds;
+}
+
+void TimeSetter::set_range(int min_s, int max_s)
+{
+    set_min_time(min_s);
+    set_max_time(max_s);
+}
+
+std::array<int, 3> TimeSetter::compute_time(int seconds)
+{
+    std::array<int, 3> time;
+    
+    int hours = (seconds / 3600);
+    int minutes = ((seconds / 60) - (hours * 60));
+    seconds -= (hours * 3600) + (minutes * 60);
+    
+    time = {hours, minutes, seconds};
+    return time;
+}
+
+int TimeSetter::compute_seconds(std::array<int, 3> time)
+{
+    return time[2] + time[1] * 60 + time[0] * 3600;
+}
+
+int TimeSetter::get_seconds()
+{
+    return compute_seconds(get_time());
+}
+
+std::array<int, 3> TimeSetter::get_time()
+{
+    return {
+            int(hours.get_value()), 
+            int(minutes.get_value()), 
+            int(seconds.get_value())
+            };
+}
+
+int TimeSetter::get_min()
+{
+    return this -> min_s;
+}
+int TimeSetter::get_max()
+{
+    return this -> max_s;
+}
--- src/gui/widgets.h
+++ src/gui/widgets.h
@@ -11,20 +11,25 @@ class TimeSetter : public Gtk::Box
         TimeSetter();
         ~TimeSetter();
         
-        void set_time(int seconds);
-        void set_limit(int min_s, int max_s);
+        void set_seconds(int seconds);
+        void set_range(int min_s, int max_s);
         void set_min_time(int seconds);
         void set_max_time(int seconds);
         
-        int get_time();
+        int get_seconds();
+        std::array<int, 3> get_time();
+        int get_min();
+        int get_max();
         
     protected:
-        int value;
-    
         Gtk::SpinButton hours, minutes, seconds;
         Glib::RefPtr<Gtk::Adjustment> a_hours, a_minutes, a_seconds;
         Gtk::Label sep1, sep2;
         
+        int min_s, max_s;
+        
         std::array<int, 3> compute_time(int seconds);
         int compute_seconds(std::array<int, 3> time);
+        void update_adjustments();
+        void resolve_overflow(Gtk::SpinButton * widget);
 };
