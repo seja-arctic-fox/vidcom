2799b9813e4d3cbe08972e1fa010ff86d4c971d9
Seja <seja.czstudio@gmail.com>
2026-05-23 18:30:48 +0200

    Prepare for release

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -21,7 +21,7 @@ MainWindow::MainWindow()
     set_default_size(960, 540);
     gtk_window_set_titlebar(GTK_WINDOW(gobj()), gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0));
     // Jen pro BETA verze: 
-    add_css_class("devel");
+    // add_css_class("devel");
     
     // Stránka pro prázdnou frontu
     add_videos_pill_button.add_css_class("pill");
@@ -173,12 +173,29 @@ void MainWindow::display_about_dialog(const Glib::VariantBase&)
     auto dialog = ADW_ABOUT_DIALOG(adw_about_dialog_new());
     
     adw_about_dialog_set_application_name(dialog, "VidCom");
-    adw_about_dialog_set_version(dialog, "0.82 Beta");
+    adw_about_dialog_set_version(dialog, "0.82");
     adw_about_dialog_set_developer_name(dialog, "seja-arctic-fox");
     adw_about_dialog_set_application_icon(dialog, "io.github.seja_arctic_fox.vidcom");
     adw_about_dialog_set_website(dialog, "https://seja-arctic-fox.github.io/");
     adw_about_dialog_set_issue_url(dialog, "https://github.com/seja-arctic-fox/vidcom/issues");
     adw_about_dialog_set_license_type(dialog, GTK_LICENSE_GPL_3_0);
+    adw_about_dialog_set_release_notes_version(dialog, "0.82");
+    adw_about_dialog_set_release_notes(dialog, 
+        "<ul>"
+            "<li>Multiple stream and subtitle support, switching between mp4 and mkv containers as needed</li>"
+            "<li>Faster seeking when Cut Feature is enabled</li>"
+            "<li>Fix to correctly compute bitrate when Cut Feature is enabled</li>"
+            "<li>Audio is now encoded in Archive mode as well. Previously it was just copied</li>"
+            "<li>New cut widget and time setters</li>"
+            "<li>UI rework to fit more into the GNOME ecosystem</li>"
+            "<li>Switch to GNOME 50 Runtime</li>"
+            "<li>Status pages for 'empty queue' and 'encoding' states</li>"
+            "<li>Improved page for viewing results, which does not create a popup window anymore</li>"
+            "<li>Popup messages changed to toasts</li>"
+            "<li>Small UI desing adjustments; rounded thumbnail corners, better info distribution, formatting bugs</li>"
+            "<li>UI refactor</li>"
+        "</ul>"
+        );
     
     adw_dialog_present(ADW_DIALOG(dialog), GTK_WIDGET(gobj()));
 }
